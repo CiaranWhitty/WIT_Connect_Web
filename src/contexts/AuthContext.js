57b51981bb3908bb56ab05454e043edb5cd46d11
Ciaran Whitty <whittyciaran1998@gmail.com>
2021-01-04 21:51:12 +0000

    Full Firebase Authentication

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,66 @@
+import React, { useContext, useState, useEffect } from "react"
+import { auth } from "../firebase/firebase"
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+const AuthProvider = (props) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+    
+  }
+
+  function logout() {
+    setTimeout(() => setIsAuthenticated(false), 100);
+    return auth.signOut();
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email)
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)      
+
+    })
+
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser,
+    isAuthenticated,
+    setIsAuthenticated,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updatePassword
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider;
--- src/index.js
+++ src/index.js
@@ -1,43 +1,63 @@
-import React from "react";
+import React, { lazy, Suspense  } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom"
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"
 
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'semantic-ui-css/semantic.min.css'
+import "./index.css";
 
-import SiteHeader from './components/siteHeader'
+import PrivateRoute from "./routes/PrivateRoute";
+import AuthProvider from "./contexts/AuthContext";
+import SiteHeader from "./components/siteHeader";
 import HomePage from "./pages/homePage";
-import mapPage from './pages/mapPage';
-
-import signupPage from "./components/SignUp";
-import LoginPage from "./components/login";
-import PrivateRoute from "./components/authentication/privateRoute";
-import AuthProvider from "./components/authentication/authContext";
-
+import signupPage from './pages/signUp'
+import loginPage from './pages/login'
+import ForgotPassword from "./pages/forgotPassword";
+import UpdateProfile from "./components/userFunction/UpdateProfile";
+const Dashboard = lazy(() => import("./pages/dashboard"));
 
 const App = () => {
-  return (
-<BrowserRouter>
-  <AuthProvider>
-    
-      <div className="jumbotron">
-        <SiteHeader />
-        
-        <div className="container-fluid">
-          <Switch>
-              
-              <Route path="/map" component={mapPage} />
-              <Route path="/signup" component={signupPage} />
-              <Route path="/login" component={LoginPage} />
-              <Route path="/" component={HomePage} />
 
-              <Redirect from="*" to="/" />
-            </Switch>
+  return (
+  <>
+  
+    <div className="content">
+      <BrowserRouter>
+      <AuthProvider>
+          <div className="jumbotron">
+            <SiteHeader />
+            <div className="container-fluid">
+                  <Suspense fallback={<h1>Loading page....</h1>}>
+                    <Switch>    
+                      
+                      {/* When logged in */}
+                      <PrivateRoute exact path="/u/dashboard" component={Dashboard} />
+                      <PrivateRoute exact path="/u/update-profile" component={UpdateProfile} />
+                      <PrivateRoute exact path="/u/" component={HomePage} />
+                      
+                      {/* Normal view */}
+                      <Route path="/forgotpassword" component={ForgotPassword} />
+                      <Route path="/login" component={loginPage} />
+                      <Route path="/signup" component={signupPage} />
+                    
+                      <Route path="/" component={HomePage} /> 
+                      <Redirect from="*" to="/" />
+
+                    </Switch>
+                  </Suspense>
+            </div>
           </div>
-          
-        </div>
-    </AuthProvider>
-</BrowserRouter>
-  );
+          </AuthProvider>
+        </BrowserRouter>
+      </div>
+    {/* Background animation - https://codepen.io/chris22smith/pen/RZogMa */}
+    <div className="bg"></div>
+    <div className="bg bg2"></div>
+    <div className="bg bg3"></div>
+
+  </>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
