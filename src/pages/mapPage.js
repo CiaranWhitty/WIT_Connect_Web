e4924f147179ccebdd363265c763ae83a15b1bc5
Ciaran Whitty <whittyciaran1998@gmail.com>
2021-02-04 19:31:55 +0000

    Fixing Aws Amplify error

--- src/pages/mapPage.js
+++ src/pages/mapPage.js
@@ -1,45 +1,36 @@
 import React from "react";
-import WITMaps from '../components/witMaps'
-import Timetable from '../components/witTimetable'
+import WITMaps from "../components/witMaps";
+import Timetable from "../components/WITTimetable";
 import { useAuth } from "../contexts/AuthContext";
 
-import { Container, Segment } from 'semantic-ui-react'
+import { Container, Segment } from "semantic-ui-react";
 
 export default function MapPage() {
- 
   const context = useAuth();
-  
+
   return context.isAuthenticated ? (
-    
-  //When logged in
+    //When logged in
     <Container fluid>
-          
       <Segment.Group>
         <Segment>
           <WITMaps />
         </Segment>
       </Segment.Group>
-      
+
       <Segment.Group>
         <Segment>
-          <Timetable/>
+          <Timetable />
         </Segment>
       </Segment.Group>
-
     </Container>
-
   ) : (
-
-  //When logged out
+    //When logged out
     <Container fluid>
-              
       <Segment.Group>
         <Segment>
           <WITMaps />
         </Segment>
       </Segment.Group>
-
     </Container>
-    
   );
-};
+}
