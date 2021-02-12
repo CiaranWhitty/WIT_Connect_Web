d2c11e64a63be52f719daf3ba8639feed9149c09
Ciaran Whitty <whittyciaran1998@gmail.com>
2021-02-12 21:25:51 +0000

    More Editing to Homepage, Removed Background

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,26 +1,37 @@
 import React from "react";
-import { Segment, Header, Grid, Container } from "semantic-ui-react";
-import BigPicture from "../components/pageComponents/bigPicture";
+import {
+  Segment,
+  Header,
+  Grid,
+  Container,
+  Button,
+  Card,
+} from "semantic-ui-react";
 
 const homePage = () => {
   return (
     <>
+      <div className="bgimg-1">
+        <div className="bgimg-2">
+          <Card id="pictureCenterContent" centered fluid >
+            <Card.Content>
+              <Card.Header>WIT Connect</Card.Header>
+              <Card.Description>
+                Welcome to WIT Connect, Enjoy your stay.
+              </Card.Description>
+              <Button         
+              variant="link"
+              size="big"
+              color="green"
+              >Learn More Here
+              </Button>
+            </Card.Content>
+          </Card>
+        </div>
+      </div>
+
       <Container id="homePageContainer">
         <Grid id="contentContainer" stackable>
-          <Grid.Row columns={1}>
-            <Grid.Column>
-              <Segment.Group id="segmentContainer">
-                <Segment id="segmentBigContent" basic>
-                  {/* <Header
-                    as="h2"
-                    content="Home"
-                    subheader="Picuture/Carousel"
-                  /> */}
-                  <BigPicture/>
-                </Segment>
-              </Segment.Group>
-            </Grid.Column>
-          </Grid.Row>
           <Grid.Row columns={2} id="segmentSmallContainer">
             <Grid.Column>
               <Segment.Group id="segmentContainer">
@@ -49,15 +60,6 @@ const homePage = () => {
               <Segment.Group id="segmentContainer">
                 <Segment id="segmentSmallContent" basic>
                   <Header as="h2" content="Home" subheader="Content 4" />
-                </Segment>
-              </Segment.Group>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns={1}>
-            <Grid.Column>
-              <Segment.Group id="segmentContainer">
-                <Segment id="segmentBigContent" basic>
-                  <Header as="h2" content="Home" subheader="Content 5" />
                 </Segment>
               </Segment.Group>
             </Grid.Column>
